612d82c37d123252a7117d8e9a19383613844d1d
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-11-07 20:35:17 +0100

    make ask optional

--- palumaj.c
+++ palumaj.c
@@ -5,22 +5,29 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define ASK_OPT "-?"
+
 typedef struct {
 	bool sync;
 	bool wait;
+	char * ask;
 } options;
 
 void
 help()
 {
-	printf("usage: [-s|--sync] [-w|--wait] [-h|--help]\nsync: run \"cave sync\" before upgrading\nwait: wait after upgrading (to read messages)\n");
+	printf("usage: [-s|--sync] [-w|--wait] [-a|-?|--ask] [-h|--help]\n");
+	printf("sync: run \"cave sync\" before upgrading\n");
+	printf("wait: wait after upgrading (to read messages)\n");
+	printf("ask: tell cave to ask before executing (needs a patched cave, like the one from the Keruspe overlay)\n");
+	printf("help: print this help");
 	exit(0);
 }
 
 options
 get_options(int argc, char ** argv)
 {
-	options opts = {false, false};
+	options opts = {false, false, NULL};
 	int i, j, size;
 	for (i = 1 ; i < argc ; ++i) {
 		size = strlen(argv[i]);
@@ -32,18 +39,29 @@ get_options(int argc, char ** argv)
 				opts.sync = true;
 			else if (strcmp(argv[i], "--wait") == 0)
 				opts.wait = true;
+			else if (strcmp(argv[i], "--ask") == 0)
+				opts.ask = ASK_OPT;
 			else if (strcmp(argv[i], "--help") == 0)
 				help();
 			continue;
 		}
 		for (j = 1 ; j < size ; ++j)
 		{
-			if (argv[i][j] == 's')
+			switch(argv[i][j])
+			{
+			case 's':
 				opts.sync = true;
-			else if (argv[i][j] == 'w')
+				break;
+			case 'w':
 				opts.wait = true;
-			else if (argv[i][j] == 'h')
+				break;
+			case '?':
+			case 'a':
+				opts.ask=ASK_OPT;
+				break;
+			case 'h':
 				help();
+			}
 		}
 	}
 	return opts;
@@ -59,14 +77,14 @@ cave_sync()
 }
 
 pid_t
-cave_resolve(bool sync)
+cave_resolve(options opts)
 {
 	pid_t pid = 0;
 	if (!(pid = fork()))
 	{
-		if (sync)
+		if (opts.sync)
 			waitpid(cave_sync(), NULL, 0);
-		execl("/usr/bin/cave", "cave", "resolve", "-x1?c", "-Cs", "-U", "*/*", "-d", "*/*", "-P", "*/*", "--suggestions", "ignore", "--recommendations", "ignore", "installed-slots", NULL);
+		execl("/usr/bin/cave", "cave", "resolve", "-x1c", "-Cs", "-U", "*/*", "-d", "*/*", "-P", "*/*", "--suggestions", "ignore", "--recommendations", "ignore", "installed-slots", opts.ask, NULL);
 	}
 	return pid;
 }
@@ -77,13 +95,13 @@ cave_purge(options opts)
 	pid_t pid = 0;
 	if (!(pid = fork()))
 	{
-		waitpid(cave_resolve(opts.sync), NULL, 0);
+		waitpid(cave_resolve(opts), NULL, 0);
 		if (opts.wait)
 		{
 			printf("Press any key to continue...");
 			while (getchar() != '\n');
 		}
-		execl("/usr/bin/cave", "cave", "purge", "-x?", NULL);
+		execl("/usr/bin/cave", "cave", "purge", "-x", opts.ask, NULL);
 	}
 	return pid;
 }
@@ -91,7 +109,8 @@ cave_purge(options opts)
 int
 main(int argc, char ** argv)
 {
-	waitpid(cave_purge(get_options(argc, argv)), NULL, 0);
-	execl("/usr/bin/cave", "cave", "fix-linkage", "-x", "--", "-?", "-Ca", NULL);
+	options opts = get_options(argc, argv);
+	waitpid(cave_purge(opts), NULL, 0);
+	execl("/usr/bin/cave", "cave", "fix-linkage", "-x", "--", "-Ca", opts.ask, NULL);
 	return 0;
 }
